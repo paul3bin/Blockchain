9d7fd174edd0533478210edd99fd993771a0550c
Ebin Paul <ebinpaul@outlook.com>
2020-07-09 12:43:37 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -90,3 +90,34 @@ app = Flask(__name__)
 
 # creating a blockchain
 blockchain = Blockchain()
+
+# mining the block
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratualtions, you have just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+# Getting the full blockchain
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+# Creating a request that checks the validity of the blockchain
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    response = blockchain.is_chain_valid(blockchain.chain)
+    return jsonify(response), 200
+
+# Running the app 
+app.run(host = '0.0.0.0', port=5000)
